685efbc09f14860e8221c5193e1ab59027a80a0f
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-02-12 23:56:09 -0200

    Removing unnecessaries modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,5 @@ module com.domineospring.infra {
     requires transitive com.domineospring.domain;
 
     requires spring.data.commons;
-    requires spring.boot;
-    requires spring.boot.autoconfigure;
-    requires spring.beans;
-    requires spring.context;
 
 }
